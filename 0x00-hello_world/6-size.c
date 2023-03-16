72c25cadc106797fb14453a8b65223ea87ae7b7c
Maryjane Okafor <ryjenspark@gmail.com>
2023-03-16 05:05:38 -0700

    bytesize

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <std.h>
+
+/**
+ * main - main function
+ *
+ * Return: use 0
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s), sizeof(char)\n");
+	printf("size of a int: %lu byte (s), sizeof(int)\n");
+	printf("size of a long int: %lu byte (s), sizeof (long int)\n");
+	printf("size of a long long int: %lu byte (s), sizeof (long long int)\n");
+	printf("size of a float: %lu byte (s), sizeof (float)\n");
+	return (0);
+
+}
+
