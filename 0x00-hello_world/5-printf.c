48b58c4ae48f6a43064cd22ef5af4e09d68ea36c
Maryjane Okafor <ryjenspark@gmail.com>
2023-03-16 03:57:07 -0700

    jhfj

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	 
+	return (0);
+}	
+
+
