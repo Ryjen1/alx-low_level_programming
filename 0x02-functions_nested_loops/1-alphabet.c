440d21aa8c7f81aa95aec647d1b30f68aba2d0ec
Maryjane Okafor <ryjenspark@gmail.com>
2023-03-21 06:44:47 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - main function
+  * Return: 0
+  */
+int main(void)
+{
+	for (char i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	putchar('\n');
+	return (0);
+}
