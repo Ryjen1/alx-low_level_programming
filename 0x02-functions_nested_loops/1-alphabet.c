8f43fdf4e47db4c25e6d979066322aef2f553482
Maryjane Okafor <ryjenspark@gmail.com>
2023-03-21 07:04:00 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
   * main - main function
   * Return: 0
@@ -7,7 +7,6 @@ int main(void)
 {
 	for (char i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
-	putchar('\n');
 	putchar('\n');
 	return (0);
 }
