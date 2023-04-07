10490cd4b300c4a4d84cc5af190e6fa8c023ef5d
Maryjane Okafor <ryjenspark@gmail.com>
2023-04-07 10:14:41 -0700

    checked

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
   * @argv: argument to print string
   * Return: 0
   */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("The name of this program is %s\n", argv[0]);
 	return (0);
