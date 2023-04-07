ba850cf24fc5ace1d86fdd011ca5c940d83c44cf
Maryjane Okafor <ryjenspark@gmail.com>
2023-04-07 10:07:31 -0700

    task1 checkresult

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - Print the name of the executable file
+  * @argc: argument to count
+  * @argv: argument to print string
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	printf("The name of this program is %s\n", argv[0]);
+	return (0);
+}
